374c2111e95e4edde5f8288f725b4e42a93c64cb
James <jhiggins3231@gmail.com>
2019-11-01 11:46:25 -0400

    first commit

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     // this is the local host name of your react app
     case 'localhost' || 'http://localhost:3000':
         // this is the local host name of your API
-        APIURL = 'http://localhost:3000';
+        APIURL = 'http://localhost:3000/';
         break;
     // this is the deployed react application
     case 'jh-motorwishlist.herokuapp.com/':
