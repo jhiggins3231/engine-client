1dc5d1581d4e8cc4c4506a794f6a68c52c7c89ea
James <jhiggins3231@gmail.com>
2019-11-04 11:57:05 -0500

    messing with styles

--- src/components/Wishlist/ViewOne.js
+++ src/components/Wishlist/ViewOne.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import car from '../../MotorAssets/interior.jpg';
-
+import Navbar from '../Navbar/Navbar';
 const Resize = styled.img`
     width: 100vw;
     height: auto;
@@ -16,6 +16,7 @@ const Background = styled.div`
 const ViewOne = (props) => {
     return(
         <Background>
+            <Navbar />
             <Resize src={car} />       
         </Background>
     )
