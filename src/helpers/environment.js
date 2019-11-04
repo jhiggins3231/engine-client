334ec2b142932a0f24fc8fde2833a0b97a49efc6
James <jhiggins3231@gmail.com>
2019-11-04 09:04:58 -0500

    updated APIURL

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -7,9 +7,9 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3000/';
         break;
     // this is the deployed react application
-    case 'jh-wishlistclient.herokuapp.com/':
+    case 'jh-wishlistserver.herokuapp.com/':
         // this is the full url of your deployed API
-        APIURL = 'http://jh-wishlistclient.herokuapp.com/'
+        APIURL = 'http://jh-wishlistserver.herokuapp.com/'
 }
 
 export default APIURL;
