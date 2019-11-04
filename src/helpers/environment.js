b20cbb3736242fa6082dd0563e75e310626c14e5
James <jhiggins3231@gmail.com>
2019-11-04 09:55:25 -0500

    attempt like 93

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,14 +2,14 @@ let APIURL = '';
 
 switch (window.location.hostname) {
     // this is the local host name of your react app
-    case 'localhost' || '3000':
+    case 'localhost' || 'localhost:3000/':
         // this is the local host name of your API
         APIURL = 'http://localhost:3000/';
         break;
     // this is the deployed react application
     case 'jh-wishlistclient.herokuapp.com':
         // this is the full url of your deployed API
-        APIURL = 'https://jh-wishlistserver.herokuapp.com'
+        APIURL = 'https://jh-wishlistserver.herokuapp.com/'
         break;
     default:
 }
