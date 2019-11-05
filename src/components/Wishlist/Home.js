10c2cd7b0618e8ecd57b2303f5c930602f12fa2b
James <jhiggins3231@gmail.com>
2019-11-05 14:10:17 -0500

    adding cards

--- src/components/Wishlist/Home.js
+++ src/components/Wishlist/Home.js
@@ -17,6 +17,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 
 
