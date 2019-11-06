dbd3693439db151e684dab80194164328e9fc3e3
James <jhiggins3231@gmail.com>
2019-11-06 08:57:47 -0500

    adding cards

--- src/components/Wishlist/Home.js
+++ src/components/Wishlist/Home.js
@@ -76,7 +76,7 @@ const Home = (props) => {
       <br />
       <br />
       <Grid container spacing={1}>
-    <Grid item xs={3}>
+    <Grid item xs={6}>
       <ImageGrid />  
       </Grid>
       <Grid item xs>
