d136ad3ad71d42825ebbf838fa8514af48661e65
James <jhiggins3231@gmail.com>
2019-11-06 19:10:17 -0500

    adding cards

--- src/components/Wishlist/Sidebar.js
+++ src/components/Wishlist/Sidebar.js
@@ -4,6 +4,7 @@ import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 
 
 const drawerWidth = 240;
@@ -30,13 +31,16 @@ const useStyles = makeStyles(theme => ({
   button: {
     width: '100px',
     height: '50px',
-    backgroundColor: '#B2B2B2',
-    color: '#F2f4f3',
     fontFamily: 'Playfair',
-    borderRadius: '5px',
     marginTop: '70px',
-    marginLeft: '30px',
-    alignItems: 'center'
+    marginLeft: '40px',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+
+    
   },
   header: {
     fontFamily: 'Playfair',
